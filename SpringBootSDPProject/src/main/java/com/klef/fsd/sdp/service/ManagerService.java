870094fd4a7abd6460caba4e58e48238d51e6100
shalinivadlamudi <2300033384@kluniversity.in>
2025-09-24 08:48:58 +0530

    Update ManagerService.java

--- SpringBootSDPProject/src/main/java/com/klef/fsd/sdp/service/ManagerService.java
+++ SpringBootSDPProject/src/main/java/com/klef/fsd/sdp/service/ManagerService.java
@@ -1,6 +1,7 @@
 package com.klef.fsd.sdp.service;
 
 import java.util.List;
+import java.util.Optional; // <-- Added import
 
 import com.klef.fsd.sdp.model.BookEvent;
 import com.klef.fsd.sdp.model.Event;
@@ -8,14 +9,14 @@ import com.klef.fsd.sdp.model.Manager;
 
 public interface ManagerService 
 {
-  public Manager checkmanagerlogin(String username,String password);
+   public Manager checkmanagerlogin(String username, String password);
   
-  public String addevent(Event event);
-  public List<Event> vieweventsbymanager(int mid);
+   public String addevent(Event event);
+   public List<Event> vieweventsbymanager(int mid);
   
-  public Manager getManagerById(int mid);
+   public Optional<Manager> getManagerById(int mid); // <-- Return Optional
   
-  public List<BookEvent> getbookingsbyManager(int mid);
+   public List<BookEvent> getbookingsbyManager(int mid);
   
-  public String updatebookingstatus(int id,String status);
+   public String updatebookingstatus(int id, String status);
 }
