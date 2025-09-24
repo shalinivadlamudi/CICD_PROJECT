216800ab6f269ea770143f3186a0e39d8926e396
shalinivadlamudi <2300033384@kluniversity.in>
2025-09-24 09:13:15 +0530

    Update SpringBootSdpProjectApplication.java

--- SpringBootSDPProject/src/main/java/com/klef/fsd/sdp/SpringBootSdpProjectApplication.java
+++ SpringBootSDPProject/src/main/java/com/klef/fsd/sdp/SpringBootSdpProjectApplication.java
@@ -2,13 +2,19 @@ package com.klef.fsd.sdp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootSdpProjectApplication {
+public class SpringBootSdpProjectApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSdpProjectApplication.class, args);
-		System.out.println("Project is Running............");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSdpProjectApplication.class, args);
+        System.out.println("Project is Running............");
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringBootSdpProjectApplication.class);
+    }
 }
